9c7b830d5f4007ba784263a2f062be33a2c56a9a
Bhuvnesh Kapoor <bhuvneshkapoor01@gmail.com>
2026-02-19 14:55:18 +0530

    change final

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
   }
 
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World from the GitOps + ArgoCD! First project ooo \n");
+  res.end("Hello World from the GitOps + ArgoCD! First project Final \n");
 });
 
 server.listen(PORT, () => {
